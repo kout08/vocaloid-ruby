74cdfcb2843da1a6339914e1a3d4d2afae83f8c2
kout08 <kout08@users.noreply.github.com>
2016-07-12 04:31:06 +0000

    Update GUI

--- main.rb
+++ main.rb
@@ -5,11 +5,16 @@ set :bind, '0.0.0.0'
 
 get '/' do
 	@title = 'Vocaloid Player'
-	erb :player
+	erb :top
 end
 
 get '/input_word' do
 	@title = 'Vocaloid Player'
 	@chars = ['a', 'b', 'c']
 	erb :input_word
+end
+
+get '/simple_player' do
+	@title = 'Vocaloid Player'
+	erb :player
 end
