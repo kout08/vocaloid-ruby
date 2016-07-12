731d9bc64165201049e91c348707598827aeab5c
kout08 <kout08@users.noreply.github.com>
2016-07-12 07:02:00 +0000

    Update gitignore

--- lib/__dir_test.rb
+++
@@ -1,4 +0,0 @@
-puts File.dirname(__FILE__)
-puts File.expand_path(File.dirname(__FILE__))
-puts __FILE__
-puts File.dirname(__FILE__) + "../public/output.wav"
--- lib/__smpl-translate.rb
+++
@@ -1,6 +0,0 @@
-require_relative 'translate-voice.rb'
-
-while (str = gets.chomp) != "0"
-	p str
-	p "#{Translater.voice str}"
-end
