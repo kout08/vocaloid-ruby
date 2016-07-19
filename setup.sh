cba7cda992af6d7e16c017708171e89daab9325f
kout08 <kout08@users.noreply.github.com>
2016-07-19 05:00:40 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,14 +11,38 @@ apt_install(){
 gem_install(){
 	gls=`gem list | grep $1`
 	if [ "$gls" != "" ]; then
+		echo "$1 is already exist."
+	else
+		echo "$1 is not exist. Start install."
+		gem install $1
+	fi
+}
+
+gem_uninstall(){
+	gls=`gem list | grep $1`
+	if [ "$gls" != "" ]; then
+		echo "$1 is exist. Start uninstall."
+		gem uninstall $1
+	else
+		echo "$1 is not exist."
+	fi
+}
+
+gem_install_nodoc(){
+	gls=`gem list | grep $1`
+	if [ "$gls" != "" ]; then
 		echo "$1 is already exist"
 	else
 		echo "$1 is not exist. Start install."
-		sudo gem install $1 --no-rdoc --no-ri
+		gem install $1 --no-rdoc --no-ri
 	fi
 }
 
 apt_install ruby
 gem_install wav-file
-gem_install sinatra
-gem_install sinatra-contrib
+gem_install_nodoc sinatra
+gem_install_nodoc sinatra-contrib
+apt_install libsox-fmt-all
+apt_install sox
+apt_install libchromaprint-dev
+gem_install ruby-sox
