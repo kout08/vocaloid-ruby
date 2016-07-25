733f963a11995f2cfd03f7e433fef52025d122a3
kboyama <kuboyama@sein.mse.kyutech.ac.jp>
2016-07-25 15:15:11 +0900

    change pitch

--- lib/pitch.rb
+++ lib/pitch.rb
@@ -1,11 +1,15 @@
 require "ruby-sox"
 require_relative 'translate-voice.rb'
 
-word = gets.chomp
-voice_file=Translater.voice(word)
-a=gets.to_s
-sox=Sox::Cmd.new()
-sox.add_input(voice_file)
-sox.set_output("sample_voice/test.wav")
-sox.set_effects(:pitch => a)
-sox.run
+def pitch(word,oct) 
+  sox=Sox::Cmd.new()
+  sox.add_input(Translater.voice(word))
+  sox.set_output("sample_voice/#{word}_#{oct}.wav")
+  sox.set_effects(:pitch => oct)
+  sox.run
+end
+
+word=gets.chomp
+oct=gets.chomp
+
+pitch(word,oct)
