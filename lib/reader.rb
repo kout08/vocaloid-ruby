f006b265db653e1d45fffcdf5c350744b0bcdcca
hiro404 <hiroaki@sein.mse.kyutech.ac.jp>
2016-08-10 14:18:05 +0900

    midifile to motes info

--- lib/reader.rb
+++ lib/reader.rb
@@ -1,36 +1,35 @@
-lyric_filename = "lyric.txt"
-midi_filename = "sample.mid"
-
-lyric = File.open(lyric_filename).read.split(" ")
-midi_txt = `ruby midi2text.rb #{midi_filename}`
-
-puts midi_txt
-
-print "\nselect track number :"
-
-tr_num = gets.to_i
-
-track_data = midi_txt.split("read")
-track_notes = track_data[tr_num].gsub(/\n/," ").split(":")
-
-tempo = track_data[1].split("\n")
-tempo.delete_if{|item| item !~ /tempo/}
-tempo = tempo[0].to_s.slice(/\(\w*.\w*/).sub(/\(/,"").to_f
-
-track_notes.delete_if{|item| item !~ /note/}
-track_notes.delete_if{|item| item =~ /off/ && item.end_with?(" 0")}
-track_notes.delete_if{|item| item =~ /vel 0/}
-
-notes = Array.new(track_notes.size){[]}
-
-(1..track_notes.size).each do |n|
-  notes[n-1] = track_notes[n-1].gsub(/on/,"1").gsub(/off/,"0").split(" ").delete_if{|item| item =~ /[a-z]/}.map{|item| item.to_i}
-  notes[n-1].delete_at(1)
+def read(lyric_filename, midi_filename)
+  
+  lyric = File.open(lyric_filename).read.split(" ")
+  midi_txt = `ruby midi2text.rb #{midi_filename}`
+  
+  puts midi_txt
+  
+  print "\nselect track number :"
+  
+  tr_num = gets.to_i
+  
+  track_data = midi_txt.split("read")
+  track_notes = track_data[tr_num].gsub(/\n/," ").split(":")
+  
+  tempo = track_data[1].split("\n")
+  tempo.delete_if{|item| item !~ /tempo/}
+  tempo = tempo[0].to_s.slice(/\(\w*.\w*/).sub(/\(/,"").to_f
+  
+  track_notes.delete_if{|item| item !~ /note/}
+  track_notes.delete_if{|item| item =~ /off/ && item.end_with?(" 0")}
+  track_notes.delete_if{|item| item =~ /vel 0/}
+  
+  notes = Array.new(track_notes.size){[]}
+  
+  (1..track_notes.size).each do |n|
+    notes[n-1] = track_notes[n-1].gsub(/on/,"1").gsub(/off/,"0").split(" ").delete_if{|item| item =~ /[a-z]/}.map{|item| item.to_i}
+    notes[n-1].delete_at(1)
+  end
+  
+  return notes, tempo, lyric
+  
 end
-
-puts "\ntrack data : #{notes}\n"
-puts "tempo : #{tempo} bpm\n"
-puts "lyric : #{lyric}\n"
-
+  
 # track data = [[on/off, note, velocity, length]] on:1 off:0
 # length to sec : sec = ( tempo * length ) / ( 60 * 480 )
